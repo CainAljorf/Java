cc1c896f80e32df94aa50a961360a9b78a79402f
Cain <cainmartinez2@hotmail.com>
2023-11-08 22:42:17 +0100

    Update pub 8/11

--- tema4/modules/products/utils/date_product.java
+++ tema4/modules/products/utils/date_product.java
@@ -5,7 +5,31 @@ import tema4.utils.validators;
 
 public class date_product {
 	public static dates insert_date_purchase(String message, String title){
-		dates D=null;
+		//funcion para crear fecha compra
+		boolean res = false;
+		String date_purchase = "";
+		dates D = null;
+		do {
+			date_purchase = validators.validator_string("Ingresa la de fecha compra.","Ingresa fecha");
+			res = regex.validateDate(date_purchase);
+			if (res == false) {
+				res = false;
+				JOptionPane.showMessageDialog(null, "Formato de fecha incorrecta, inténtelo de nuevo. ", "Formato", JOptionPane.WARNING_MESSAGE);
+			} else {
+				D = new dates(date_purchase);
+				if (D.check_date()) {
+					res = true;
+				}else {
+					res = false;
+				}
+				if (res == false) {
+					res = false;
+					JOptionPane.showMessageDialog(null, "Fecha no válida.", "Error", JOptionPane.ERROR_MESSAGE);
+				} else {
+					res = true;
+				} // end if
+			} // end if
+		} while ((res == false));
 		return D;
 	}//end insert_date_purchase
 }
--- tema4/test.java
+++ tema4/test.java
@@ -8,38 +8,14 @@ public class test {
 	
 	public static void main(String[] args){
 		
-		//funcion para crear fecha compra
-		boolean res = false;
-		String date_purchase = "";
-		dates D = null;
-		int num=0;
-		do {
-			date_purchase = validators.validator_string("Ingresa la de fecha compra.","Ingresa fecha");
-			res = regex.validateDate(date_purchase);
-			if (res == false) {
-				res = false;
-				JOptionPane.showMessageDialog(null, "Formato de fecha incorrecta, inténtelo de nuevo. ", "Formato", JOptionPane.WARNING_MESSAGE);
-			} else {
-				D = new dates(date_purchase);
-				if (D.check_date()) {
-					res = true;
-				}else {
-					res = false;
-				}
-				if (res == false) {
-					res = false;
-					JOptionPane.showMessageDialog(null, "Fecha no válida.", "Error", JOptionPane.ERROR_MESSAGE);
-				} else {
-					res = true;
-				} // end if
-			} // end if
-		} while ((res == false));
-		System.out.println(D.getDate_purchase());
-		//return D; //Lo llamamamos desde el crud para crear o actualizar.
+		
 
 		//funcion para crear fecha entrega,debe recoger la fecha de compra
 		String date_delivery = "";
 		String str;
+		boolean res=false;
+		dates D=null;
+		int num;
 		do {
 			date_delivery = validators.validator_string("Ingresa fecha de entrega, debe ser un día mayor a la fecha de compra."
 					+ "\nFecha de compra: "+D.getDate_purchase(),"Ingresa fecha");
